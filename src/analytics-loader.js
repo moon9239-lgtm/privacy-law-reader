45a669c384733ba905072b4297ba032605662d50
주문호 <moon9239@gmail.com>
2026-08-20 01:15:09 +0900

    Restore the public visitor badge

---
+++ src/analytics-loader.js
@@ -0,0 +1,6 @@
+import { installPublicVisitorSummary } from "./public-visitor-counter.js";
+
+if (typeof document !== "undefined" && typeof window !== "undefined") {
+  installPublicVisitorSummary(document, window);
+}
+
